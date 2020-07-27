fab740e294bb2e36004d7c5d0cb97ddc8be27696
artem chernov <artemcernov79@gmail.com>
2020-07-27 17:03:43 +0300

    Fix connection string

--- db.connection.js
+++ db.connection.js
@@ -1,4 +1,4 @@
-const connectionString = "mongodb://db/docker";
+const connectionString = "mongodb://db/dockerArtem";
 const db = require('@paralect/node-mongo').connect(connectionString);
 
 module.exports = db;
